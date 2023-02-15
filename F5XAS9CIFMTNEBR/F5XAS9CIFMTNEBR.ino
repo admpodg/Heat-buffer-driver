7d7a14e9c6f0947213e5539d09bd7e8cd1a527c8
admpodg <admpodg@gmail.com>
2023-02-15 15:39:07 +0100

    added offset values, reformated serial.print for .csv format - time to make some real life test on heat buffer

--- F5XAS9CIFMTNEBR/F5XAS9CIFMTNEBR.ino
+++ F5XAS9CIFMTNEBR/F5XAS9CIFMTNEBR.ino
@@ -24,7 +24,7 @@ DallasTemperature sensors(&oneWire); //how it works here? //remember, it's "Ardu
         union sensor_address ID;
         uint8_t sensor_physical_order_no;  
         uint8_t assigned_discovery_number;
-        //offset calibration
+        int32_t offset_calibration;
         };
 
 int sensors_count;
@@ -36,15 +36,15 @@ void setup(void)
   sensor_address tempDS18B20_address;
   
 //used sensor adressess
-  DS18B20_table[0] = {{0x28, 0x96, 0x00, 0x00, 0x9A, 0x5C, 0x00, 0x65}, 0, 0xFF}; //why extended init did not work? maybe only during init and def?//C allows nesting, c++ not, that's why
-  DS18B20_table[1] = {{0x28, 0xC8, 0x00, 0x00, 0x11, 0x63, 0x00, 0xCE}, 1, 0xFF};
-  DS18B20_table[2] = {{0x28, 0x96, 0x00, 0x00, 0x99, 0x46, 0x00, 0x8A}, 2, 0xFF};
-  DS18B20_table[3] = {{0x28, 0x96, 0x00, 0x00, 0xB2, 0x4A, 0x00, 0x92}, 3, 0xFF};
-  DS18B20_table[4] = {{0x28, 0xAF, 0x00, 0x00, 0xD8, 0x10, 0x01, 0xE8}, 4, 0xFF};
-  DS18B20_table[5] = {{0x28, 0xC8, 0x00, 0x00, 0xC6, 0x0C, 0x00, 0xEF}, 5, 0xFF};
-  DS18B20_table[6] = {{0x28, 0xC8, 0x00, 0x00, 0x6A, 0x20, 0x00, 0x2E}, 6, 0xFF};
-  DS18B20_table[7] = {{0x28, 0xC8, 0x00, 0x00, 0x31, 0x61, 0x00, 0xCB}, 7, 0xFF};
-  DS18B20_table[8] = {{0x28, 0x96, 0x00, 0x00, 0xBD, 0x64, 0x00, 0xD0}, 8, 0xFF};
+  DS18B20_table[0] = {{0x28, 0x96, 0x00, 0x00, 0x9A, 0x5C, 0x00, 0x65}, 0, 0xFF, 184}; //why extended init did not work? maybe only during init and def?//C allows nesting, c++ not, that's why
+  DS18B20_table[1] = {{0x28, 0xC8, 0x00, 0x00, 0x11, 0x63, 0x00, 0xCE}, 1, 0xFF, 144};
+  DS18B20_table[2] = {{0x28, 0x96, 0x00, 0x00, 0x99, 0x46, 0x00, 0x8A}, 2, 0xFF}, 16;
+  DS18B20_table[3] = {{0x28, 0x96, 0x00, 0x00, 0xB2, 0x4A, 0x00, 0x92}, 3, 0xFF, 88};
+  DS18B20_table[4] = {{0x28, 0xAF, 0x00, 0x00, 0xD8, 0x10, 0x01, 0xE8}, 4, 0xFF, -16};
+  DS18B20_table[5] = {{0x28, 0xC8, 0x00, 0x00, 0xC6, 0x0C, 0x00, 0xEF}, 5, 0xFF, 312};
+  DS18B20_table[6] = {{0x28, 0xC8, 0x00, 0x00, 0x6A, 0x20, 0x00, 0x2E}, 6, 0xFF, 216};
+  DS18B20_table[7] = {{0x28, 0xC8, 0x00, 0x00, 0x31, 0x61, 0x00, 0xCB}, 7, 0xFF, 144};
+  DS18B20_table[8] = {{0x28, 0x96, 0x00, 0x00, 0xBD, 0x64, 0x00, 0xD0}, 8, 0xFF, 8};
   
   // start serial port
   Serial.begin(9600);
@@ -89,22 +89,25 @@ void loop(void)
   sensors.requestTemperatures(); // Send the command to get temperatures
   Serial.println("");
 
-for (int i =0; i < sensors.getDS18Count(); i++){
-  //Serial.print("Temp_");
-  //Serial.print(i);
+for (int i =0; i <DS18B20_COUNT ; i++){
   
-  Serial.print(sensors.getTempCByIndex(DS18B20_table[i].assigned_discovery_number)); // Why "byIndex"? 
-  Serial.print("\t");
+  //Serial.print(sensors.getTempCByIndex(DS18B20_table[i].assigned_discovery_number)); // Why "byIndex"? 
+  //Serial.print("\t");
+Serial.print(sensors.getTemp(DS18B20_table[i].ID.sensor_address_byte_table));
+Serial.print(" ");
+
+  Serial.print((float) (sensors.getTemp(DS18B20_table[i].ID.sensor_address_byte_table)+DS18B20_table[i].offset_calibration)/128.f); //9bit res, 1/128 temp value
+  //Serial.print(".");
+  //Serial.print((sensors.getTemp(DS18B20_table[i].ID.sensor_address_byte_table)%128)/128);//get some value after colon, make this binary shift later
+  Serial.print(";");
 
   }
   Serial.println("");
     // You can have more than one IC on the same bus. 
     // 0 refers to the first IC on the wire
-    delay(1000);
+    delay(30000);
     //test for raw values 
-    Serial.print(sensors.getTemp(DS18B20_table[8].ID.sensor_address_byte_table)/128); //9bit res, 1/128 temp value
-    Serial.print(".");
-    Serial.print(sensors.getTemp(DS18B20_table[8].ID.sensor_address_byte_table)%128);
+    
 
 }
 
